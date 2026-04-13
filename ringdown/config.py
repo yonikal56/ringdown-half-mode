3f92ffbcca7d4f657083353f3b467bfcbfc98767
yonikal56 <yonikal56@gmail.com>
2026-04-13 10:41:51 +0300

    revert change for checking

--- ringdown/config.py
+++ ringdown/config.py
@@ -6,4 +6,4 @@ PIPE_SECTION = "pipe"
 WHITENED_LOGLIKE_KEY = "whitened_pointwise_loglike"
 
 # constants
-T_MSUN = 4.92549094764126745135e-06  # lal.GMSUN_SI / lal.C_SI**3
+T_MSUN = 4.92549094764126745136e-06  # lal.GMSUN_SI / lal.C_SI**3
